afbc0731aad2a9ac469b39a5b6a83ee70f9fb758
trinitaagustin <trinitaagustin34@gmail.com>
2024-12-04 10:41:01 +0700

    Add files via upload

---
+++ praktikum 11/Gempa.py
@@ -0,0 +1,21 @@
+class Gempa:
+    def __init__(self, skala, lokasi):
+        self.skala = skala
+        self.lokasi = lokasi
+
+    def dampak(self):
+        print (f"Ada gempa baru nih di {self.lokasi}")
+        print (f"Skala dari gempa ini adalah {self.skala}")
+        if self.skala < 2:
+            print('Dampak ga berasa')
+        elif self.skala >= 2 and self.skala <= 4:
+            print('Dampak gempa bangunan retak-retak')
+        elif self.skala > 4 and self.skala <= 6:
+            print('Dampak gempa bangunan roboh')
+        elif self.skala > 6 :
+            print('Dampak gempa bangunan roboh dan potensi tsunami')
+        else :
+            print('Sistem tidak dapat membaca')
+            
+#gempa1 = Gempa(5, "Jawa Barat")
+#gempa1.dampak()
